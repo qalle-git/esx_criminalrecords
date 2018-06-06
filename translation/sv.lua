c5a0430b4f0c4e73e97722b48ce19766313d8352
Calle <36276161+qalleh1@users.noreply.github.com>
2018-06-06 22:10:02 +0200

    no locales in this

--- translation/sv.lua
+++ translation/sv.lua
@@ -1,8 +1,2 @@
 Locales['sv'] = {
-  -- vanliga
-  	['duty'] = 'Tryck ~INPUT_CONTEXT~ för att gå ~g~in~s~/~r~ur~s~ tjänst',
-	['onduty'] = 'Du gick in i tjänst.',
-	['offduty'] = 'Du gick ur tjänst.',
-	['notpol'] = 'Du är ej inom poliskåren.',
-	['notamb'] = 'Du är ej inom sjukvården.',
 }
